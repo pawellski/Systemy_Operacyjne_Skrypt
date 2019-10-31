e537122f8e93b6ed2e0e3af8335e995ab08cced2
Paweł <skibaapawell@gmail.com>
2019-10-31 13:21:27 +0100

    Stworzenie skryptu

---
+++ skrypt.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+
+REM ****************************** 
+REM * 	      PAWEŁ SKIBA        *
+REM *  SKRYPT POWŁOKI TEKSTOWEJ  *
+REM *  	   LABORATORIUM NR 3     *
+REM *      SYSTEMY OPERACYJNE    *
+REM ******************************
+
