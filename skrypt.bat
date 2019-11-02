5d9fa0ba0c348f9c65567ff8f94f4a328234cfe8
Paweł <skibaapawell@gmail.com>
2019-11-03 00:54:31 +0100

    Zliczanie liczby arg oraz info o powloce

--- skrypt.bat
+++ skrypt.bat
@@ -7,3 +7,23 @@ REM *  	   LABORATORIUM NR 3     *
 REM *      SYSTEMY OPERACYJNE    *
 REM ******************************
 
+Title skrypt.bat
+
+goto :checkNumberOfArguments
+:returnCheckNumberOfArguments
+
+
+
+ECHO --------------------------------------------------------------------------
+ECHO Wartosc zmiennej COMSPEC: %COMSPEC%
+ECHO Uzywana powloka:
+for %%1 in (%COMSPEC%) do echo %%~nx1
+ECHO --------------------------------------------------------------------------
+EXIT /B 0
+
+
+:checkNumberOfArguments
+set numberOfArguments=0
+for %%x in (%*) do set /A numberOfArguments +=1
+echo Liczba argumentow wejsciowych %numberOfArguments%
+goto :returnCheckNumberOfArguments
